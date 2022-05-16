899f38b084cc3795f50fd62b16591e803d776b23
Thomas Schaaf <thomas.schaaf@komola.de>
2022-05-16 14:01:58 +0200

    show that tests are working on v1

--- src/components/CustomTaskList/CustomTaskList.tsx
+++ src/components/CustomTaskList/CustomTaskList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Manager } from '@twilio/flex-ui'
 import { CustomTaskListComponentStyles } from './CustomTaskList.Styles';
 import { StateToProps, DispatchToProps } from './CustomTaskList.Container';
 
@@ -12,6 +13,8 @@ type Props = StateToProps & DispatchToProps & OwnProps;
 
 // It is recommended to keep components stateless and use redux for managing states
 const CustomTaskList: React.FunctionComponent<Props> = (props: Props) => {
+  console.log(Manager)
+
   if (!props.isOpen) {
     return null;
   }
